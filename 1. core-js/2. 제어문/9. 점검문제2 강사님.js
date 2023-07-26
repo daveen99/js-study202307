e7958de08c611ae5ac246a4243064f6987704b1f
daveen99 <prospecs1039@daum.net>
2023-07-26 10:12:26 +0900

    core js - 강사님 updown게임 . 난이도 추가

--- 1. core-js/2. 제어문/9. 점검문제2 강사님.js
+++ 1. core-js/2. 제어문/9. 점검문제2 강사님.js
@@ -1,5 +1,34 @@
 
 
+
+// 난이도 상수
+const HIGH = 1;
+const MIDDLE = 2;
+const LOW = 3;
+
+
+// 초기 카운트 수
+let initCount;
+
+// 초기 난이도 설정 선택창 띄우기
+let level = +prompt('난이도를 선택하세요!\n# [1. 상(3번의 기회) | 2. 중(6번의 기회) | 3. 하(10번의 기회)]');
+if (level === HIGH){
+    initCount = 3;
+}
+else if (level === MIDDLE){
+    initCount = 6;
+}
+else if (level === LOW){
+    initCount = 10;
+}
+else {
+    alert(`Auto level system online, set level[Hell]`);
+    initCount = 1;
+}
+
+// 카운트다운 변수
+let countdown = initCount;
+
 // 실제로 정답으로 사용할 랜덤 정수 만들기
 let secret = Math.floor(Math.random() * 100) + 1;
 console.log(secret);
@@ -7,10 +36,6 @@ console.log(secret);
 // 입력 최소, 최대값
 let min = 1, max = 100;
 
-// 초기 카운트 수
-let initCount = 5;
-// 카운트다운 변수
-let countdown = initCount;
 
 while (true) {
   //사용자의 정답 입력값
@@ -28,7 +53,7 @@ while (true) {
   //업 다운 판단
   //정답인 경우
   if (secret === answer) {
-    alert(`정답입니다.` );
+    alert(`정답입니다. ${initCount - countdown}번 만에 맞추셨어요.`);
     break;
   } else if (secret > answer) {
     alert(`UP`);
