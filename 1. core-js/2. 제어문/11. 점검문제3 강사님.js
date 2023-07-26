24d975d5279fcd57c671259907f4d9b2af81f80c
daveen99 <prospecs1039@daum.net>
2023-07-26 10:16:45 +0900

    core js - 강사님 사칙연산게임. 정답오답횟수 출력

--- 1. core-js/2. 제어문/11. 점검문제3 강사님.js
+++ 1. core-js/2. 제어문/11. 점검문제3 강사님.js
@@ -1,4 +1,10 @@
+// 문제 넘버링
 let qNumber = 1;
+
+// 정답, 오답 횟수
+let correctCount = 0;
+let wrongCount = 0;
+
 while (true) {
   // 랜덤 정수 2개를 생성
   let firstNumber = Math.floor(Math.random() * 20) + 1;
@@ -16,7 +22,11 @@ while (true) {
   // 정답 확인
   if (userAnswer === firstNumber + secondNumber) {
     alert(`정답입니다!`);
+    correctCount++;
   } else {
     alert(`틀렸습니다.`);
+    wrongCount++;
   }
 }
+
+alert(`정답횟수: ${correctCount}, 오답횟수: ${wrongCount}`);
