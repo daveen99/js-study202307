be08e00efb82ca4fdf3aac4b7b8c0e85f4137973
daveen99 <prospecs1039@daum.net>
2023-08-02 13:28:03 +0900

    web api- 실전예제1: 로그 렌더링 기능 추가

--- 2. web-api/App/Calculator/assets/scripts/app.js
+++ 2. web-api/App/Calculator/assets/scripts/app.js
@@ -1,7 +1,42 @@
 // index.html에서 활용할 이벤트처리, 핸들러 ...
 
+// 연산타입을 상수로 관리
+const ADD = 'ADD';
+const SUB = 'SUB';
+const MULTI = 'MULTI';
+const DIVIDE = 'DIVIDE';
+
+
+
 // 계산 이력을 모아둘 배열
 const logEntries = [];
+
+// operation 문자로 기호를 생성하는 함수
+const convertOperationToMark = (operation) => {
+  let mark;
+  switch(operation) {
+    case ADD : mark = '+';break;
+    case SUB : mark = '-';break;
+    case MULTI : mark = '*';break;
+    case DIVIDE : mark = '/';break;
+  }
+  return mark;
+};
+
+
+let seq = 0;
+// 로그 이력을 렌더링하는 함수
+const renderToLog = ({operation, prevResult:firstNumber, number:secondNumber, result}) => {
+    // li태그 생성
+    const $newLi = document.createElement('li');
+    $newLi.classList.add('log-entries__item');
+    
+    // ul에 추가
+    $logEntries.appendChild($newLi);
+    $newLi.textContent = `#${++seq}. ${firstNumber} ${convertOperationToMark(operation)} ${secondNumber} = ${result}`;
+};
+
+
 // 로그 이력을 만드는 함수
 const writeToLog = (operation, prevResult, number, result) => {
   // 하나의 로그 객체 (연산, 이전결과, 연산숫자, 연산결과)
@@ -13,6 +48,8 @@ const writeToLog = (operation, prevResult, number, result) => {
   };
   logEntries.push(logObject);
   console.log(logEntries);
+
+  renderToLog(logObject);
 };
 
 // 현재 계산기에 그려질 숫자
@@ -28,7 +65,7 @@ const createLogAndRenderOutput = (operateMark, originResult, calcNumber) => {
   // 화면에 렌더링
   outputResult(currentResult, calcDscriptionLog);
 };
-
+// let cnt =0;
 // 계산 기능 헬퍼 함수
 const calculate = (type) => {
   // 계산 전 값을 백업
@@ -37,15 +74,15 @@ const calculate = (type) => {
   const enteredNumber = getUserNumberInput();
 
   let mark;
-  if (type === 'ADD') {
+  if (type === ADD) {
     mark = '+';
     // 실제 계산결과 반영
     currentResult += enteredNumber;
-  } else if (type === 'SUB') {
+  } else if (type === SUB) {
     mark = '-';
     // 실제 계산결과 반영
     currentResult -= enteredNumber;
-  }else if (type === 'MULTI') {
+  }else if (type === MULTI) {
     mark = '*';
     // 실제 계산결과 반영
     currentResult *= enteredNumber;
@@ -60,23 +97,30 @@ const calculate = (type) => {
 
   // 로그 이력 쌓기
   writeToLog(type, originResult, enteredNumber, currentResult);
+
+//   cnt += 1;
+//   const $newLi = document.createElement('li');
+//   $newLi.textContent = `${cnt}. ` + logEntries[logEntries.length - 1].prevResult + ` ${mark} ` +
+//   logEntries[logEntries.length - 1].number + " = " + logEntries[logEntries.length - 1].result;
+
+//   $logUl.appendChild($newLi);
 };
 
 // 더하기 버튼 이벤트 핸들러
 const addHandler = () => {
-  calculate('ADD');
+  calculate(ADD);
 };
 // 빼기 버튼 이벤트 핸들러
 const subtractHandler = () => {
-    calculate('SUB');
+    calculate(SUB);
 };
 // 곱하기 버튼 이벤트 핸들러
 const multiplyHandler = () => {
-    calculate('MULTI');
+    calculate(MULTI);
 };
 // 나누기 버튼 이벤트 핸들러
 const divideHandler = () => {
-    calculate('DIVIDE');
+    calculate(DIVIDE);
 };
 
 // =================== 이벤트 핸들러 바인딩 =====================//
--- 2. web-api/App/Calculator/assets/scripts/vendor.js
+++ 2. web-api/App/Calculator/assets/scripts/vendor.js
@@ -10,7 +10,8 @@ const $divideBtn = document.getElementById('btn-divide');
 const $currentResultOutput = document.getElementById('current-result');
 const $currentCalculationOutput = document.getElementById('current-calculation');
 
-
+// const $logUl = document.getElementById(`log`);
+const $logEntries = document.querySelector('.log-entries');
 function outputResult(result, text) {
   $currentResultOutput.textContent = result;
   $currentCalculationOutput.textContent = text;
