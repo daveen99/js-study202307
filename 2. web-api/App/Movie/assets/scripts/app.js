bf4fa384ee5449545bc0bca7832becea16020d86
daveen99 <prospecs1039@daum.net>
2023-08-03 17:51:42 +0900

    web api- 실전예제2: 기능추가

--- 2. web-api/App/Movie/assets/scripts/app.js
+++ 2. web-api/App/Movie/assets/scripts/app.js
@@ -1,6 +1,6 @@
 // 모달 엘리먼트
 const $addMovieModal = document.getElementById("add-modal");
-const $deleteMovieModal = document.getElementById('delete-modal');
+const $deleteMovieModal = document.getElementById("delete-modal");
 
 // 영화 추가 버튼
 const $addMovieButton = document.querySelector("header button");
@@ -11,25 +11,48 @@ const $cancelAddMovieButton = $addMovieModal.querySelector(".btn--passive");
 const $confirmAddMovieButton = $cancelAddMovieButton.nextElementSibling;
 
 // 영화 추가 모달 안에 있는 입력엘리먼트들
-const $userInputs = [
-  ...$addMovieModal.querySelectorAll("input")
-];
+const $userInputs = [...$addMovieModal.querySelectorAll("input")];
 const [$titleInput, $imgUrlInput, $ratingInput] = $userInputs;
-const $entryTextSection = document.getElementById('entry-text');
+const $entryTextSection = document.getElementById("entry-text");
 
 // 영화목록 ul태그
-const $movieList = document.getElementById('movie-list');
+const $movieList = document.getElementById("movie-list");
 
-const CLASS_VISIBLE = 'visible';
+const CLASS_VISIBLE = "visible";
+const CLASS_INVISIBLE = "invisible";
 
+const $entry = document.getElementById("entry-text");
+const $delModal = document.getElementById("delete-modal");
+
+const $cancleDeleteMovieButton = document.querySelector(".canclebtn");
+const $confirmDeleteMovieButton = document.querySelector(".confirmbtn");
 // 영화 정보 목록 배열
 const movies = [];
 
+let movieId = 0;
+let index = 0;
+let etarget = 0;
+let etarget2 = 0;
+
+
+const $liInputTitle = document.querySelector('.modal__content #title');
+const $liInputImage = document.querySelector('.modal__content #image-url');
+const $liInputRating = document.querySelector('.modal__content #rating');
+
 // ===== 유틸함수, 일반함수 정의 ===== //
+
+// entry-text를 보이게/안보이게 하는 함수
+const entryInvisible = () => {
+  if (movies.length >= 1) {
+    $entry.classList.add(CLASS_INVISIBLE);
+  } else {
+    $entry.classList.remove(CLASS_INVISIBLE);
+  }
+};
 
 // 모든 인풋을 리셋하는 함수
 const clearMovieModalInput = () => {
-  $userInputs.forEach($input => $input.value = '');
+  $userInputs.forEach(($input) => ($input.value = ""));
 };
 
 // 영화추가모달을 닫는 함수
@@ -39,10 +62,21 @@ const closeAddModal = () => {
   clearMovieModalInput();
 };
 
+const closeDelModal = () => {
+  $delModal.classList.remove(CLASS_VISIBLE);
+};
+const showDelModal = (e) => {
+  $delModal.classList.add(CLASS_VISIBLE);
+};
+// const checkup = () => {
+//   realDel();
+//   closeDelModal();
+// };
+
 // 화면에 새로운 영화 정보를 렌더링하는 함수
 const renderNewMovie = ({ id, title, image, rating }) => {
-  const $newMovie = document.createElement('li');
-  $newMovie.classList.add('movie-element');
+  const $newMovie = document.createElement("li");
+  $newMovie.classList.add("movie-element");
   $newMovie.dataset.movieId = id;
 
   $newMovie.innerHTML = `
@@ -56,11 +90,11 @@ const renderNewMovie = ({ id, title, image, rating }) => {
   `;
 
   // 삭제를 진행하는 핸들러
-  const deleteMovieHandler = e => {
-
+  const deleteMovieHandler = (e) => {
+    showDelModal();
     // 배열에서도 영화 정보를 지워야 함!
     // 클릭한 태그의 근처 li의 movie-id값 가져오기
-    const movieId = e.target.closest('.movie-element').dataset.movieId;
+    movieId = e.target.closest(".movie-element").dataset.movieId;
     console.log(movieId);
 
     // 배열에서 해당 아이디값을 가지는 객체를 찾아내고 인덱스를 알아내기
@@ -74,40 +108,50 @@ const renderNewMovie = ({ id, title, image, rating }) => {
 
     // 대상의 인덱스 찾기
     // indexOf: 원시타입 (숫자, 문자열)만 찾을 수 있음
-    // findIndex: 배열 고차함수 
-    const index = movies.findIndex(m => m.id === movieId);
+    // findIndex: 배열 고차함수
+    index = movies.findIndex((m) => m.id === movieId);
     console.log(`클릭대상 인덱스: ${index}`);
 
-    // 그 객체를 배열에서 지우기 <- ?? : 인덱스를 알아야 지우지
-    movies.splice(index, 1);
-    
-    // 실제 li 지우기
-    e.target.closest('.movie-element').remove();
+    // // 그 객체를 배열에서 지우기 <- ?? : 인덱스를 알아야 지우지
+    // movies.splice(index, 1);
+
+    // // 실제 li 지우기
+    etarget = e.target.closest('.movie-element');
+    entryInvisible();
   };
 
   // 삭제 클릭 이벤트
-  $newMovie.addEventListener('click', deleteMovieHandler)
+  $newMovie.addEventListener("click", deleteMovieHandler);
 
   $movieList.appendChild($newMovie);
+
+  entryInvisible();
+};
+const realDel = () => {
+  movies.splice(index, 1);
+  etarget.remove();
+  closeDelModal();
+  entryInvisible();
 };
 
-// 영화 정보 입력란 검증 
-const validateMovieInput = ({title, image, rating}) => {
+// 영화 정보 입력란 검증
+const validateMovieInput = ({ title, image, rating }) => {
   if (
-    title.trim() === '' || 
-    image.trim() === '' || 
-    rating.trim() === '' ||
-    +rating < 1 || +rating > 5
+    title.trim() === "" ||
+    image.trim() === "" ||
+    rating.trim() === "" ||
+    +rating < 1 ||
+    +rating > 5
   ) {
     return false;
   }
   return true;
-}
+};
 
 // =====  이벤트 핸들러 및 이벤트 바인딩 ===== //
 
 // 영화 추가버튼 기능을 수행하는 핸들러
-const addMovieHandler = e => {
+const addMovieHandler = (e) => {
   const titleValue = $titleInput.value; // 제목입력란값
   const imgUrlValue = $imgUrlInput.value; // 이미지경로
   const ratingValue = $ratingInput.value; // 평점입력값
@@ -117,12 +161,12 @@ const addMovieHandler = e => {
     id: Math.random().toString(),
     title: titleValue,
     image: imgUrlValue,
-    rating: ratingValue
+    rating: ratingValue,
   };
 
   // 검증
   if (!validateMovieInput(newMovie)) {
-    alert('입력값이 유효하지 않습니다!');
+    alert("입력값이 유효하지 않습니다!");
     return;
   }
 
@@ -137,30 +181,43 @@ const addMovieHandler = e => {
 };
 
 // 영화 추가 모달창을 띄우는 핸들러
-const showMovieModalHandler = e => {
+const showMovieModalHandler = (e) => {
   $addMovieModal.classList.add(CLASS_VISIBLE);
   $backdrop.classList.add(CLASS_VISIBLE);
 };
 
-
-
 // 백드롭 영역을 클릭하면 모달이 닫히는 핸들러
-const backdropHandler = e => {
+const backdropHandler = (e) => {
   closeAddModal();
 };
 // 영화 추가 모달창의 취소버튼을 누르면 모달이 닫히는 핸들러
-const closeMovieModalHandler = e => {
+const closeMovieModalHandler = (e) => {
   closeAddModal();
 };
 
 // Add movie버튼 클릭이벤트
-$addMovieButton.addEventListener('click', showMovieModalHandler);
+$addMovieButton.addEventListener("click", showMovieModalHandler);
 
 // backdrop영역 클릭이벤트
-$backdrop.addEventListener('click', backdropHandler);
+$backdrop.addEventListener("click", backdropHandler);
+
+const confirmAddEnterHandler = e => {
+  if (e.key === 'Enter') {
+    $confirmAddMovieButton.click(); // 프로그래밍적으로 클릭명령
+  }
+};
+
 
 // Add Movie모달 취소버튼 클릭이벤트
-$cancelAddMovieButton.addEventListener('click', closeMovieModalHandler);
-
+$cancelAddMovieButton.addEventListener("click", closeMovieModalHandler);
 // Add Movie모달 추가버튼 클릭이벤트
-$confirmAddMovieButton.addEventListener('click', addMovieHandler);
+$confirmAddMovieButton.addEventListener("click", addMovieHandler);
+
+// delete 모달 취소버튼 클릭 이벤트
+$cancleDeleteMovieButton.addEventListener("click", closeDelModal);
+// delete 모달 확인버튼 클릭 이벤트
+$confirmDeleteMovieButton.addEventListener("click", realDel);
+
+$liInputTitle.addEventListener('keyup', confirmAddEnterHandler);
+$liInputImage.addEventListener('keyup', confirmAddEnterHandler);
+$liInputRating.addEventListener('keyup', confirmAddEnterHandler);
