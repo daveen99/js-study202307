cf4b52d8b5e589e3e1131221f67008b4b7f4b412
daveen99 <prospecs1039@daum.net>
2023-08-09 13:31:56 +0900

    ajax- 실전예제 강사님버전 최종

--- 3. ajax/TodoList/assets/scripts/app.js
+++ 3. ajax/TodoList/assets/scripts/app.js
@@ -8,24 +8,57 @@ const $todoUl = document.querySelector(".todo-list");
 const $addbtn = document.getElementById("add");
 //수정버튼
 
-fetch(URL)
-  .then((res) => res.json())
-  .then((todo_list) => {
-    // console.log(todo_list);
+const fetchTodos = (url, method='GET', payload=null) => {
+  const requestInit = {
+    method: method,
+    headers: { 'Content-Type': 'application/json' }
+  };
+  if (payload) requestInit.body = JSON.stringify(payload);
 
-    const $TodoTemplate = document.getElementById("todo-template");
+  return fetch(url, requestInit);
+};
 
-    todo_list.forEach(({ id, text, done }) => {
-      const $todoLi = document.importNode($TodoTemplate.content, true);
+// fetch(URL)
+//   .then((res) => res.json())
+//   .then((todo_list) => {
+//     // console.log(todo_list);
 
-      $todoLi.querySelector("li").dataset.postId = id;
-      $todoLi.querySelector(".text").textContent = text;
-      $todoLi.querySelector("input").textContent = done;
-      // $todoLi.querySelector(".checkbox").textContent = done;
+//     const $TodoTemplate = document.getElementById("todo-template");
 
-      $todoUl.appendChild($todoLi);
-    });
+//     todo_list.forEach(({ id, text, done }) => {
+//       const $todoLi = document.importNode($TodoTemplate.content, true);
+
+//       $todoLi.querySelector("li").dataset.postId = id;
+//       $todoLi.querySelector(".text").textContent = text;
+//       $todoLi.querySelector("input").textContent = done;
+//       // $todoLi.querySelector(".checkbox").textContent = done;
+
+//       $todoUl.appendChild($todoLi);
+//     });
+//   });
+
+// 화면에 todos를 렌더링하는 함수
+const renderTodos = (todoList) => {
+  // li태그의 템플릿을 가져옴
+  const $liTemplate = document.getElementById('todo-template');
+
+  todoList.forEach(({ id, text, done }) => {
+    // console.log('todo: ', todo);
+    const $newLi = document.importNode($liTemplate.content, true);
+    $newLi.querySelector('li').dataset.id = id; // data-id 속성 부여
+    $newLi.querySelector('.text').textContent = text;
+
+    // 체크박스 가져오기
+    const $checkbox = $newLi.querySelector('.checkbox input[type=checkbox]');
+    // console.dir($checkbox);
+    $checkbox.checked = done;
+
+    done && $checkbox.parentNode.classList.add('checked');
+
+    $todoUl.appendChild($newLi);
+    console.log($todoUl);
   });
+};
 
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -54,19 +87,33 @@ const deletePostHandler = (e) => {
   e.preventDefault();
   if (e.target.matches(".modify span")) {
     // const id = e.target.closest(".todo-list-item").dataset.postId;
+    const id = e.target.closest('.todo-list-item').dataset.id;
     const $addtext = e.target.parentNode.parentNode.querySelector('#addtext');
     const $beforemodi = e.target.parentNode.parentNode.querySelector('.text');
-    console.log($addtext);
-    console.log($beforemodi);
-
+    console.log('before',$beforemodi.textContent);
+    const $word = $addtext.value;
+    // if (!$addtext.value === null) {
+    //   $beforemodi.textContent = $addtext.value;
+    //   return;
+    // }
     $addtext.value = $beforemodi.textContent;
+    // console.log('add',$addtext.value);
+    // console.log(getText);
+    // $addtext.value = getText;
+    // console.log($addtext.value);
 
+    console.log($word);
     if ($addtext.classList.value === "modify-input") {
-      $beforemodi.textContent = $addtext.value;
-      $addtext.value = null;
+      $beforemodi.textContent = $word;
+      $addtext.classList.add("visible");
       $addtext.classList.remove("modify-input");
       $beforemodi.classList.remove("visible");
+
+      fetchTodos(`${URL}/${id}`, 'PATCH', {
+        text: $word 
+      });
     } else {
+      $addtext.classList.remove("visible");
       $addtext.classList.add("modify-input");
       $beforemodi.classList.add("visible");
     }
@@ -87,42 +134,55 @@ const deletePostHandler = (e) => {
   });
 };
 
-$todoUl.addEventListener(
-  "change",
-  (e) => {
-    e.preventDefault();
-    if (!e.target.matches(".checkbox input")) return;
 
-    // console.log('?:', e.target);
+// $todoUl.addEventListener(
+//   "change",
+//   (e) => {
+//     e.preventDefault();
+//     if (!e.target.matches(".checkbox input[type=checkbox]")) return;
 
-    const id = e.target.closest(".todo-list-item").dataset.postId;
-    let payload;
-    if (e.target.checked) {
-      payload = {
-        done: true,
-      };
-    } else {
-      payload = {
-        done: false,
-      };
-    }
-    fetch(`${URL}/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
-    // .then((res) => res.json())
-    // .then((data) => console.log(data))
-  },
-  true
-);
+//     // console.log('?:', e.target);
 
-const modifyHandler = (e) => {};
+//     const id = e.target.closest(".todo-list-item").dataset.postId;
+//     let payload;
+//     if (e.target.checked) {
+//       payload = {
+//         done: true,
+//       };
+//     } else {
+//       payload = {
+//         done: false,
+//       };
+//     }
+//     fetch(`${URL}/${id}`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(payload),
+//     });
+//     // .then((res) => res.json())
+//     // .then((data) => console.log(data))
+//   },
+//   true
+// );
+
+// const modifyHandler = (e) => {};
 
 // .catch(err => {
 //   console.log(err);
 // });
 
 $todoUl.addEventListener("click", deletePostHandler);
+
+
+// =========== 앱 실행 =========== //
+const init = () => {
+  fetchTodos(URL)
+    .then(res => res.json())
+    .then(todos => {
+      renderTodos(todos);
+    });
+};
+
+init();
